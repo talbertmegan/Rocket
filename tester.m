dce2d94f3595832c33fc84daa68ab23cb63742fc
RiddledEnigma <iangreen2.00@gmail.com>
2019-03-20 21:11:41 -0700

    just a test for syncing computers

--- tester.m
+++ tester.m
@@ -1,10 +1,11 @@
 
 
-system = [...
-        [0,-6.371e6,0],5.972e24, [0,0,0];... %earth
-        [0,383.4e6,0],7.3276e22, [-1.022e3,0,0]... %moo
-    ];
+%lets get JAVA to work
+strcat(pwd, "/gui")
+javaaddpath( pwd)
 
-system = [system;system;system]
 
-system = updateSystem(system)
+javaaddpath("gui")
+
+o = wtf.jar;
+
