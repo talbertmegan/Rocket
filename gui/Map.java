f1c868245414e3c334f7caf154fd13bd8a6d2e55
Megan <talbertmegan@gmail.com>
2019-04-28 11:16:22 -0700

    Map.java work for panel

--- gui/Map.java
+++ gui/Map.java
@@ -3,6 +3,7 @@ import java.awt.*;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+//import java.awt.Graphics;
 
 
 public class Map extends GUI_PANEL_SUPER { //GUI panel super adds some setters automatically
@@ -33,9 +34,19 @@ public class Map extends GUI_PANEL_SUPER { //GUI panel super adds some setters a
     }
 
     public void MapDraw(Double[] RocketLocation, Double[] MoonLocation){
+      Graphics graphics = panel.getGraphics();
       graphics.fillEllipse(position[1][1],position[2][1],5,10);//rocket Ellipse
       graphics.fillEllipse(position[1][2],position[2][2],100,100);//moon Ellipse
       graphics.fillEllipse(0,0,200,200);//Earth ellipse
+    }
+
+    public void main(){
+      JFrame frame = new JFrame();
+      DrawingPanel panel = new DrawingPanel();
+      panel.setPrefferedSize(new Dimension(500, 500));
+      frame.add(panel);
+      frame.pack();
+      frame.setVisible(true);
     }
 
 }
